d3f8b832045d5ae8597cab6edad7ccc95f285bdd
Julian Millen <julimillen@gmail.com>
2025-05-01 20:08:23 -0300

    Property Binding y Two way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,15 @@ import { InterpolacionComponent } from "./components/interpolacion/interpolacion
 import { PadreComponent } from "./components/padre/padre.component";
 import { HijoComponent } from "./components/hijo/hijo.component";
 import { MostrarMensajeComponent } from "./components/mostrar-mensaje/mostrar-mensaje.component";
+import { ReplicadorComponent } from "./components/replicador/replicador.component";
+import { SaludarComponent } from "./components/saludar/saludar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteEnLineaComponent, InterpolacionComponent, PadreComponent, HijoComponent, MostrarMensajeComponent],
+  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteEnLineaComponent, InterpolacionComponent, PadreComponent, HijoComponent, MostrarMensajeComponent, ReplicadorComponent, SaludarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Hola mundo';
+  title = 'Two way Binding en Angular';
 }
