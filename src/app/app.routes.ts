ec29cdd58998b7c5b9d5f51a50394a6c2a8f6f7c
Julian Millen <julimillen@gmail.com>
2025-09-27 18:38:20 -0300

    comenzando con routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ListadoUsuariosComponent } from './components/listado-usuarios/listado-usuarios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: ListadoUsuariosComponent },
+];
