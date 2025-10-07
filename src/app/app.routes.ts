b1f6017912c100bb5db3a2d06b7797a4e3d5a073
Julian Millen <julimillen@gmail.com>
2025-10-07 18:24:33 -0300

    navegacion entre rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { ListadoUsuariosComponent } from './components/listado-usuarios/listado-usuarios.component';
 import { MostrarMensajeComponent } from './components/mostrar-mensaje/mostrar-mensaje.component';
+import { PadreComponent } from './components/padre/padre.component';
+import { HijoComponent } from './components/padre/hijo/hijo.component';
+
 
 export const routes: Routes = [
     {path:'', component: ListadoUsuariosComponent },
-    {path:'mostrar-mensaje', component:MostrarMensajeComponent}
+    {path:'mostrar-mensaje', component:MostrarMensajeComponent},
+    {
+        path:'configuracion', children: [
+        {path: 'padre', component: PadreComponent}, //localhost:4200/configuracion/padre
+        {path: 'hijo', component:HijoComponent} //localhost:4200/configuracion/hijo
+    ]}
 ];
